88f879d609fdfcf2d5ca9ab875cd6d06e074d202
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-07-25 03:45:51 +0600

    First commit

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@
 
 const Loading = () => {
   return (
-    <div className="bg-black text-white w-full h-[100%] absolute z-50 flex justify-center items-center">
+    <div className="bg-black text-white w-full h-[100%] absolute z-50 flex justify-center items-center top-0 left-0">
         PLEASE WAIT APP IS LOADING.....
     </div>
   )
